a3740140e699a6f76be250935aabff7fcf212f86
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2017-09-09 14:04:35 +0100

    add basic rss feed to app

---
+++ headlines.py
@@ -0,0 +1,35 @@
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+BBC_FEEDS = {
+    "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
+    "cnn": "http://rss.cnn.com/rss/edition.rss",
+    "fox": "http://feeds.foxnews.com/foxnews/latest",
+    "iol": "http://www.iol.co.za/cmlink/1.640"
+}
+
+@app.route('/')
+@app.route('/<source>')
+def get_news(source="bbc"):
+    feed = feedparser.parse(BBC_FEEDS[source])
+    first_article = feed['entries'][0]
+    return """
+        <html>
+            <body>
+                <h1>Headlines</h1>
+                <b>{0}</b><br />
+                <b>{1}</b><br />
+                <p>{2}</p><br />
+            </body>
+        </html>
+    """.format(
+        first_article.get("title"),
+        first_article.get("published"),
+        first_article.get("summary")
+    )
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
